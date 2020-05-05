970550c0b9ecca51c42342e088e947ae4bc7f066
Natalia Titova <bnmlucky@gmail.com>
2020-05-05 16:42:54 -0400

    rewrote getArea function as an arrow function

--- js/arrow-functions.js
+++ js/arrow-functions.js
@@ -2,3 +2,8 @@ const getRandomNumber = (upper) => {
   const randomNumber = Math.floor(Math.random() * upper) + 1;
   return randomNumber;
 }
+
+const getArea = (width, length, unit) => {
+  const area = width * length;
+  return `${area} ${unit}`;
+};
